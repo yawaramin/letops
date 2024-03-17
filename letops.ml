33701627d6d4e18beae06de2ce15dbc3d6493ee9
Yawar Amin <yawar.amin@gmail.com>
2024-03-17 00:32:39 -0400

    Initial commit

---
+++ letops.ml
@@ -0,0 +1,54 @@
+module In_channel = struct
+  let ( let& ) inc f = Fun.protect ~finally:(fun () -> In_channel.close inc) f
+end
+
+module List = struct
+  let ( let* ) l f = l |> List.map f |> List.flatten
+  let ( let+ ) l f = List.map f l
+
+  let ( and+ ) param1 param2 =
+    let* val1 = param1 in
+    let+ val2 = param2 in
+    (val1, val2)
+end
+
+(*
+module Mutex = struct
+  let ( let& ) = Mutex.protect
+end
+*)
+
+module Option = struct
+  let ( let* ) = Option.bind
+  let ( let+ ) o f = Option.map f o
+
+  let ( and+ ) param1 param2 =
+    let* val1 = param1 in
+    let+ val2 = param2 in
+    (val1, val2)
+end
+
+module Out_channel = struct
+  let ( let& ) outc f =
+    Fun.protect ~finally:(fun () -> Out_channel.close outc) f
+end
+
+module Result = struct
+  let ( let* ) = Result.bind
+  let ( let+ ) r f = Result.map f r
+
+  let ( and+ ) param1 param2 =
+    let* val1 = param1 in
+    let+ val2 = param2 in
+    (val1, val2)
+end
+
+module Seq = struct
+  let ( let* ) s f = Seq.flat_map f s
+  let ( let+ ) s f = Seq.map f s
+
+  let ( and+ ) param1 param2 =
+    let* val1 = param1 in
+    let+ val2 = param2 in
+    (val1, val2)
+end
---
+++ letops.mli
@@ -0,0 +1,31 @@
+module In_channel : sig
+  val ( let& ) : in_channel -> (unit -> 'a) -> 'a
+end
+
+module List : sig
+  val ( let* ) : 'a list -> ('a -> 'b list) -> 'b list
+  val ( let+ ) : 'a list -> ('a -> 'b) -> 'b list
+  val ( and+ ) : 'a list -> 'b list -> ('a * 'b) list
+end
+
+module Option : sig
+  val ( let* ) : 'a option -> ('a -> 'b option) -> 'b option
+  val ( let+ ) : 'a option -> ('a -> 'b) -> 'b option
+  val ( and+ ) : 'a option -> 'b option -> ('a * 'b) option
+end
+
+module Out_channel : sig
+  val ( let& ) : out_channel -> (unit -> 'a) -> 'a
+end
+
+module Result : sig
+  val ( let* ) : ('a, 'b) result -> ('a -> ('c, 'b) result) -> ('c, 'b) result
+  val ( let+ ) : ('a, 'b) result -> ('a -> 'c) -> ('c, 'b) result
+  val ( and+ ) : ('a, 'b) result -> ('c, 'b) result -> ('a * 'c, 'b) result
+end
+
+module Seq : sig
+  val ( let* ) : 'a Seq.t -> ('a -> 'b Seq.t) -> 'b Seq.t
+  val ( let+ ) : 'a Seq.t -> ('a -> 'b) -> 'b Seq.t
+  val ( and+ ) : 'a Seq.t -> 'b Seq.t -> ('a * 'b) Seq.t
+end
